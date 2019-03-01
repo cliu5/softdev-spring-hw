26c7b3bdaf3935d6af22fe510bef26bc8b8ece2e
cliu5 <cliu5@stuy.edu>
2019-03-01 12:19:33 -0500

    refactored

--- 06_mongo/mango.py
+++ 06_mongo/mango.py
@@ -1,21 +1,30 @@
 import pymongo
 from pymongo import MongoClient
-client=MongoClient()
-db=client.test
 
-data=db['primer-dataset']
+SERVER_ADDR="167.99.158.108"
+connection = pymongo.MongoClient(SERVER_ADDR)
+db = connection.test
+collection = db.restaurants
+
+def get_borough(borough):
+    return collection.find( {'borough': borough} )
+
+def get_zipcode(zipcode):
+    return collection.find( {'address.zipcode': zipcode} )
+
+def get_zip_grade(zipcode, grade):
+    return collection.find( {'address.zipcode': zipcode, 'grades.grade': grade} )
+
+def get_zip_score(zipcode, score):
+    return collection.find( {'address.zipcode': zipcode, 'grades.score': {'$lt': score} } )
+
+def display_restaurants(collection):
+    for i in collection:
+        print(i)
 
 
-def borough(b):
-    return data.find({"borough":b})
-
-def zipcode(z):
-    return data.find({"address.zipcode":(z)})
-
-def zipgrade(z,g):
-    return data.find({"$and":[{"address.zipcode":z},{"grades.grade": g}]})
-
-def score(s):
-    return data.find({"grades.score":{'$lt':s}})
-
-
+display_restaurants(get_borough("Brooklyn"))
+display_restaurants(get_zipcode("11364"))
+display_restaurants(get_zip_grade("11364", "B"))
+display_restaurants(get_zip_score("11364", 5))
+display_restaurants(get_zip_grade_score("11364", "C", 20))
